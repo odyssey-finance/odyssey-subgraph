907aa34ffce3ac0a9c663965ce3539a0aba273b2
Rohit Solia <rohit.solia@gmail.com>
2025-10-30 10:54:34 -0500

    Added support for primary token oracle

--- config/optimism/address.ts
+++ config/optimism/address.ts
@@ -2,3 +2,5 @@ import { Address } from '@graphprotocol/graph-ts'
 
 export const POSITION_REGISTRY = Address.fromString('0xeE156D8ea7b96a5524CcC3CF9283ab85E80E9534')
 export const MASTER_ORACLE = Address.fromString('0x0aac835162D368F246dc71628AfcD6d2930c47d3')
+// NOTE: Subgraph expects lowercase addresses, so make sure to use token-oracle pairs with lowercase addresses.
+export const TOKEN_ORACLES = new Map<string, string>()
